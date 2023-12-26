9c2855bac3537dc029498773109f742ea6b738d2
Abdulla Hassan <abdullahassan052@gmail.com>
2023-12-26 04:51:15 +0300

    Responsive for all screen sizes, and disappear toggle in small screen

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const registerBtn = document.getElementById('register');
 
 const loginBtn = document.getElementById('login');
 
+const signUpSwitch = document.querySelector('.js-sign-up-switch');
+const loginSwitch = document.querySelector('.js-login-switch');
+
 
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
@@ -11,4 +14,16 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
+})
+
+//For switching forms in smaller screen
+
+loginSwitch.addEventListener('click', () => {
+    const signUp = document.querySelector('.sign-up');
+    signUp.style.zIndex = '6';
+})
+
+signUpSwitch.addEventListener('click', () => {
+    const signUp = document.querySelector('.sign-up');
+    signUp.style.zIndex = '1';
 })
