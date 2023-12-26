9d2854b7dad477c2a9146d18befe5ae5c2b0faff
Abdulla Hassan <abdullahassan052@gmail.com>
2023-12-26 14:27:27 +0300

    Solve the bug in responsive design (Sign Up form resizing)

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const loginBtn = document.getElementById('login');
 const signUpSwitch = document.querySelector('.js-sign-up-switch');
 const loginSwitch = document.querySelector('.js-login-switch');
 
+const signUp = document.querySelector('.sign-up');
+const signIn = document.querySelector('.sign-in');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
@@ -18,12 +20,29 @@ loginBtn.addEventListener('click', () => {
 
 //For switching forms in smaller screen
 
+
+// *I think I solved
 loginSwitch.addEventListener('click', () => {
-    const signUp = document.querySelector('.sign-up');
-    signUp.style.zIndex = '6';
+    signUp.style.zIndex = '7';
+    signIn.style.zIndex = '6';
 })
 
 signUpSwitch.addEventListener('click', () => {
-    const signUp = document.querySelector('.sign-up');
-    signUp.style.zIndex = '1';
+    signUp.style.zIndex = '6';
+    signIn.style.zIndex = '7';
 })
+
+window.addEventListener('resize', () => {
+    if (window.screen.width >= 670) {
+        //Back to its main position for bigger screens
+        signUp.style.transform = 'translateX(100%)';
+        console.log("Greater");
+    }
+    else {
+        //Both in same position but had different z-index
+        signIn.style.transform = 'translateX(0%)';
+        signUp.style.transform = 'translateX(0%)';
+        console.log("Less");
+    }
+});
+
